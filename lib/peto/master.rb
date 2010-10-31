990c0e7f1f64f3b45330f67288328bbcd6d988f2
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-11-01 00:39:48 +0900

    template file cannot find at installed gem. fixed it.

--- lib/peto/master.rb
+++ lib/peto/master.rb
@@ -3,6 +3,7 @@ require "yaml"
 require "peto/generator"
 
 module Peto
+  TEMPLATE_DIR = File::dirname( File::expand_path( __FILE__ ) ) + "/../templates"
   class Master
     def load(filename)
       @contract = YAML.load(IO.read(Pathname(filename)))
@@ -10,7 +11,7 @@ module Peto
     attr_reader :contract
 
     def generate
-      Generator.new(@contract).generate("lib/templates/rb_procedures.erb")
+      Generator.new(@contract).generate(TEMPLATE_DIR + "/rb_procedures.erb")
     end
   end
 end
